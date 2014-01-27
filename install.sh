3a4b755473026cd56ae96d23143122f2c06ef3a4
Josh Gillies <github@joshgilli.es>
2014-01-27 17:41:37 +1100

    Update install.sh
    Update install script for Ubuntu 12.04 LTS

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 apt-get install -y software-properties-common
+apt-get install -y python-software-properties
 add-apt-repository -y ppa:chris-lea/node.js
 apt-get update
 apt-get install -y nodejs
